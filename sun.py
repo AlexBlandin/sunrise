a72781d3efdde54c9f5b36624220d84c13506fa0
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-05-29 02:44:13 +0100

    Switched

---
+++ sun.py
@@ -0,0 +1,4 @@
+from algo1 import algo1 as sun
+
+if __name__ == "__main__":
+  print(sun())
--- test-rig.py
+++ test-rig.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from algo1 import algo1
 from algo2 import algo2
-from sunrise import sun
+from correct import sun
 from helpers import day_after
 
 swansea = 51.62, 3.95
 when = datetime.now()
-print("Almanac", sun(*swansea, when))
+print("Correct", sun(*swansea, when))
 print("Algo 01", algo1(*swansea, when))
 print("Algo 02", algo2(*swansea, when))
 
-# Algo1 is basically as accurate as Almanac, it just may be a minute off a little under half the time.
+# Algo1 is basically as accurate as Correct, it just may be a minute off a little under half the time.
